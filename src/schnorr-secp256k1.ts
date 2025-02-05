e7aad8180fcf7ad44393391cf851b63e6cbad9e8
0x471 <0x471@protonmail.com>
2025-02-05 00:59:17 +0000

    feat(wip): incomplete schnorr bip340 verification

--- src/schnorr-secp256k1.ts
+++ src/schnorr-secp256k1.ts
@@ -3,14 +3,23 @@ import {
   Crypto,
   CanonicalForeignField,
   ForeignCurve,
+  Poseidon,
+  Bool,
+  UInt32,
+  UInt64,
+  Bytes,
+  AlmostForeignField,
+  ForeignField,
+  assert,
 } from 'o1js';
+import { FlexiblePoint } from 'o1js/dist/node/lib/provable/crypto/foreign-curve';
 
 // Define Secp256k1 curve
 class Secp256k1Curve extends createForeignCurve(Crypto.CurveParams.Secp256k1) {}
 
-export { SchnorrSecp256k1, Secp256k1Curve };
+export { SchnorrBIP340Secp256k1, Secp256k1Curve };
 
-class SchnorrSecp256k1 {
+class SchnorrBIP340Secp256k1 {
   // Generator point G
   private static G = Secp256k1Curve.generator;
 
@@ -23,11 +32,53 @@ class SchnorrSecp256k1 {
     privateKey: CanonicalForeignField;
     publicKey: ForeignCurve;
   } {
-    // Generate random private key
+    // Generate: a random private key
     const privateKey = Secp256k1Curve.Scalar.random();
-    // Compute public key as P = dG
-    const publicKey = SchnorrSecp256k1.G.scale(privateKey);
+    // Calculate: P = d * G
+    const publicKey = SchnorrBIP340Secp256k1.G.scale(privateKey);
 
     return { privateKey, publicKey };
+  }
+
+  // https://github.com/bitcoin/bips/blob/master/bip-0340
+  /**
+   * Verifies a Schnorr signature (BIP430)
+   *
+   * @param {FlexiblePoint} publicKeyPoint - The public key P must be a valid x-coordinate lifted to a curve point `lift_x` ensures we have a valid curve point by computing the corresponding y-coordinate and choosing the even y-value as per BIP340/Schnorr specification
+   * @param {AlmostForeignField} messageHash - Original message
+   * @param {Object} signature - Signature containing R and s
+   * @returns {Bool} True if signature is valid, False otherwise
+   */
+  static verify(
+    publicKeyPoint: FlexiblePoint,
+    messageHash: AlmostForeignField,
+    signature: {
+      r: AlmostForeignField;
+      s: AlmostForeignField;
+    }
+  ): Bool {
+    const curve = ForeignCurve.from(publicKeyPoint);
+    
+    // Calculate: sG = s * G
+    const sG = SchnorrBIP340Secp256k1.G.scale(signature.s);
+
+    // e = Hash(R.x || P.x || m)
+    const e = messageHash;
+
+    // Calculate: eP = e * P
+    const eP = curve.scale(e);
+
+    // Calculate: R = sG - eP
+    const R = sG.add(eP.negate());
+
+    // TODO, Check: is_infinite(R). Does o1js handle this check?
+
+    // Check: R has even y-coordinate
+    const isEvenY = R.y.toBits()[0].equals(Bool(true));
+
+    // Check: R.x == r
+    const hasMatchingX = R.x.equals(signature.r.toBigInt());
+
+    return isEvenY.and(hasMatchingX);
   }
 }
